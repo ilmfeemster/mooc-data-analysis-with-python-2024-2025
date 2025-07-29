a91223bf5f6b5b202c46b3e53ed372fc5bc2ece1
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2025-07-28 21:36:50 -0700

    multiplication_table completed

--- part01-e04_multiplication_table/src/multiplication_table.py
+++ part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,10 @@
 
 
 def main():
-    pass
+    for row in range(1, 11):
+        for col in range(1, 11):
+            print("{:4d}".format(row * col), end="")
+        print("")
 
 if __name__ == "__main__":
     main()
