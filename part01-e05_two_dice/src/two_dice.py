e16ea1f06a690a6502ed75826b58cf77260d80bc
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2025-07-29 17:40:34 -0700

    two_dice completed

--- part01-e05_two_dice/src/two_dice.py
+++ part01-e05_two_dice/src/two_dice.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    for die1 in range(1,7):
+        for die2 in range(1,7):
+            if (die1 + die2 == 5):
+                print(f"({die1}, {die2})")
 
 if __name__ == "__main__":
     main()
