1e22b821138043b1be11a3ac4b043c6fb42f2746
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2025-07-29 18:29:20 -0700

    triple_square completed

--- part01-e06_triple_square/src/triple_square.py
+++ part01-e06_triple_square/src/triple_square.py
@@ -2,7 +2,21 @@
 
 
 def main():
-    pass
+    number = 1
+    while True:
+        tripled = triple(number)
+        squared = square(number)
+        if (tripled < squared):
+            break
+        print(f"triple({number})=={tripled} square({number})=={squared}")
+        number += 1
+        
+
+def triple(number):
+    return number * 3
+    
+def square(number):
+    return number ** 2
 
 if __name__ == "__main__":
     main()
