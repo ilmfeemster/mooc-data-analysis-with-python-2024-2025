3b83ee8b97d52f29f29b7c5b2e81f7fc262cd7e5
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2025-07-29 18:41:21 -0700

    areas_of_shapes completed

--- part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -2,9 +2,33 @@
 
 import math
 
+def triangle_area():
+    base = input("Give the base of the triangle: ")
+    height = input("Give the height of the triangle: ")
+    print("The area is {:6f}".format(0.5 * float(base) * float(height)))
+
+def rectangle_area():
+    width = input("Give the width of the rectangle: ")
+    height = input("Give the height of the rectangle: ")
+    print("The area is {:6f}".format(float(width) * float(height)))
+
+def circle_area():
+    radius = input("Give the radius of the circle: ")
+    print("The area is {:6f}".format(math.pi * float(radius) ** 2))
 
 def main():
-    # enter you solution here
+    while True:
+        shape = input("What shape do you want to calculate the area for? (triangle/rectangle/circle/exit): ").strip().lower()
+        if shape == "triangle":
+            triangle_area()
+        elif shape == "rectangle":
+            rectangle_area()
+        elif shape == "circle":
+            circle_area()
+        elif shape == "":
+            break
+        else:
+            print("Unknown shape!")
 
 if __name__ == "__main__":
     main()
